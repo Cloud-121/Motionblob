f04188c1e776b3d928f59926998d7a195b0dfd00
Cloud Hayes <Cloud@gproconnect.com>
2025-06-05 12:01:36 -0500

    helper script

---
+++ quicklogs.sh
@@ -0,0 +1,12 @@
+#Simple script just to quickly setup the virtual env and run the logging script
+# It's just here to make my life easier x3
+
+if [ -d "venv" ]; then
+    . venv/bin/activate
+else
+    python -m venv venv
+    . venv/bin/activate
+    python -m pip install -r requirments.txt
+fi
+
+python pull.py
